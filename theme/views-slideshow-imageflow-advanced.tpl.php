a165fd97a6df88fb7afee96cdd0da12be8e450f1
Alberto Trujillo Gonzalez <altrugon@gmail.com>
2012-04-24 10:04:59 -0700

    Update view slideshow template to include new fields.

--- theme/views-slideshow-imageflow-advanced.tpl.php
+++ theme/views-slideshow-imageflow-advanced.tpl.php
@@ -16,19 +16,39 @@
     foreach ($images as $key => $image) {
       print  $image ."\n";
       $record=$view->result[$key];
-      $caption='<div class="species-commonname">'.$record->node_title.'</div>';
+      $caption='<h3 class="species-commonname">'.$record->node_title.'</h3>';
+      
+      // Scientific names
+      if (isset($record->field_field_species_name_scientific[0]) && !empty($record->field_field_species_name_scientific[0]['rendered']['#markup'])) {
+        $caption .= '<div class="species-scientificname"><span class="field-label">Scientific Name:&nbsp;</span>';
+        $caption .= $record->field_field_species_name_scientific[0]['rendered']['#markup'];
+        $caption .= '</div>';
+      }
 
+      // Other names
+      if (isset($record->field_field_species_name_other) && !empty($record->field_field_species_name_other)) {
+        $caption .= '<div class="species-othername"><span class="field-label">Other Name(s):&nbsp;</span>';
+        $o_names = array();
+        foreach ($record->field_field_species_name_other as $o_name) {
+          $o_names[] = $o_name['rendered']['#markup'];
+        }
+        $caption .= implode(', ', $o_names);
+        $caption .= '</div>';
+      }
+
+      // Translation
       foreach ($record->field_field_cis_translation as $trans_parent) {
         #dpm ($trans_parent, "translation parent");
         $trans_item=$trans_parent['rendered']['entity']['field_collection_item'][$trans_parent['raw']['value']];
         #dpm ($trans_item, "translation item");
         if ($language=$trans_item['field_cis_translation_lang'][0]['#title'] AND $name=$trans_item['field_cis_translation_name'][0]['#markup']) {
-          $caption .= '<div class="species-translation">' .$language. ': '.$name;
+          $caption .= '<table class="species-translation"><tr>';
+          $caption .= '<td class="field-value"><span class="field-label">' .$language. ':&nbsp;</span> ' . $name . '</td>';
           if (isset($trans_item['field_cis_translation_audio'][0]['#file']->uri)) {
             $audio='/'.variable_get('file_public_path', conf_path() . '/files').'/'.substr($trans_item['field_cis_translation_audio'][0]['#file']->uri,9);
-            $caption.= '<span class="species-play">'.cis_species_playbutton($audio).'</span>';
+            $caption.= '<td class="species-play">'.cis_species_playbutton($audio).'</td>';
           }
-          $caption .= '</div>';
+          $caption .= '</tr></table>';
         }
       }
       $desc[]=$caption;
