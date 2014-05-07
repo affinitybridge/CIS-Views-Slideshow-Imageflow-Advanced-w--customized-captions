4d675cd1ad06211116954a5603820e64228e195c
Shiraz Dindar <shiraz@affinitybridge.com>
2014-05-07 10:18:58 -0700

    refs #1360. Update species slideshow to use archive versions of the audio and video translation files

--- theme/views-slideshow-imageflow-advanced.tpl.php
+++ theme/views-slideshow-imageflow-advanced.tpl.php
@@ -17,7 +17,7 @@
       print  $image ."\n";
       $record=$view->result[$key];
       $caption='<h3 class="species-commonname">'.$record->node_title.'</h3>';
-      
+
       // Scientific name
       if (isset($record->field_field_species_name_scientific[0]) && !empty($record->field_field_species_name_scientific[0]['rendered']['#markup'])) {
         $caption .= '<div class="species-scientificname"><span class="field-label">Scientific Name:&nbsp;</span>';
@@ -35,7 +35,7 @@
         $caption .= '<span class="field-value">' . implode(', ', $o_names) . '</span>';
         $caption .= '</div>';
       }
-      
+
       // Species use
       if (count($record->field_field_cis_species_use)>0) {
         $caption .= '<div class="species-speciesuse"><span class="field-label">Species Use:&nbsp;</span> <span class="field-value">';
@@ -45,42 +45,47 @@
         $caption = substr($caption,0,-2);
         $caption .= '</span></div>';
       }
-      
+
       // Public notes
       if (isset($record->field_field_cis_species_public_notes [0]) && !empty($record->field_field_cis_species_public_notes [0]['rendered']['#markup'])) {
         $caption .= '<div class="species-notes"><span class="field-label">Notes:&nbsp;</span>';
         $caption .= '<span class="field-value">' . $record->field_field_cis_species_public_notes[0]['rendered']['#markup'] . '</span>';
         $caption .= '</div>';
       }
-      
+
        // Translation
       foreach ($record->field_field_cis_translation as $index => $trans_parent) {
         $trans_item=$trans_parent['rendered']['entity']['field_collection_item'][$trans_parent['raw']['value']];
         if (isset($trans_item['field_cis_translation_lang'][0]['#title']) AND isset($trans_item['field_cis_translation_name'][0]['#markup'])) {
           $language=$trans_item['field_cis_translation_lang'][0]['#title'];
           $name=$trans_item['field_cis_translation_name'][0]['#markup'];
-          
+
           $caption .= '<table class="species-translation"><tr>';
           $caption .= '<td class="field-label">' . $language . ':&nbsp</td>';
           $caption .= '<td class="field-value">' . $name;
-          
+
           //audio file
-          if (isset($trans_item['field_cis_translation_audio'][0]['#attributes']['src'])) {
-            $caption.= '<div class="species-play">'.cis_species_playbutton($trans_item['field_cis_translation_audio'][0]['#attributes']['src']).'</div>';
+          if (isset($trans_item['field_ckk_translation_arc_audio'])) {
+            $archive = $trans_item ['field_ckk_translation_arc_audio']['#items'][0]['entity'];
+            $audio_priv_path = $archive->field_cis_arc_attach_audio[LANGUAGE_NONE][0]['uri'];
+            $audiopath = file_create_url($audio_priv_path);
+            $caption .= '<div class="species-play">' . cis_species_playbutton($audiopath) . '</div>';
           }
-          
+
           // video popup
-          if (isset($trans_item['field_cis_translation_video'][0]['#attributes']['src'])) {
-            $vidpath=$trans_item['field_cis_translation_video'][0]['#attributes']['src'];
-            
+          if (isset($trans_item['field_ckk_translation_arc_video'])) {
+            $archive = $trans_item ['field_ckk_translation_arc_video']['#items'][0]['entity'];
+            $vid_priv_path = $archive->field_cis_arc_attach_video[LANGUAGE_NONE][0]['uri'];
+            $vidpath = file_create_url($vid_priv_path);
+
             // lightbox solution
             //$caption .= '<td>'.l(t('video'), $vidpath, array('attributes' => array('rel' => 'lightvideo[group][' . $language. ': ' . $name . ']'))) .'</td>';
-            
+
             // video tag solution
             $vid = 'dialog-nid' . $record->nid . '-vid' . $trans_parent['raw']['value'];
-            
+
             /* Note: the l() function returns "/#$vid" for the url and I just want "#$vid"
-            
+
             $link = l(t('video'), '',
                 array(
                   'attributes' => array('class' => 'dialog-opener'),
@@ -89,7 +94,7 @@
               ) .
             */
             $link = '<a href="' . $vidpath . '" class="dialog-opener" title="' . $language . '">' . t('video') . '</a>';
-            
+
             /*
             $video_dialog = '<div id="' . $vid . '" class="display-in-dialog" title="' . $language . '">' .
               theme('mediaelement_video',
@@ -106,18 +111,18 @@
               ) .
             '</div>';
             */
-            
+
             $caption .= '<div class="vide-wrapper">' . $link . /*$video_dialog .*/ '</div>';
             // end of: video tag solution
-            
+
           }
           $caption  .= '</td>';
           $caption .= '</tr></table>';
         }
-        
+
       }
       $desc[]=$caption;
-      
+
     } ?>
   </div>
 </div>
